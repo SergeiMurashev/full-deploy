392b90f5ba33d30dfc9463eef7e33e21de4f80d5
Сергей <sssrrr15v@gmail.com>
2025-03-17 14:56:23 +0500

    Implementation of the app deployment

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Full Stack Application</h1>
+        <p>This is a React frontend with a Go backend</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
